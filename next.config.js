832a61b224c22a8e3189ebd482bd466c00624394
NivekNK <kevin.rojas.v@mail.pucv.cl>
2022-07-04 17:19:40 -0400

    Fixes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    basePath: procces.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: procces.env.NEXT_PUBLIC_BASE_PATH
 }
--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,4 @@ const Page = () => {
     )
 }
 
-
 export default Page
