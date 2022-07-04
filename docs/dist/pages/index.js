cd1994d9d944b3ab79d926f5c72b90da55539cb6
NivekNK <kevin.rojas.v@mail.pucv.cl>
2022-07-04 16:13:46 -0400

    New Start

--- docs/dist/pages/index.js
+++
@@ -1,3 +0,0 @@
-import React from "../../snowpack/pkg/react.js";
-import ReactDOM from "../../snowpack/pkg/react-dom.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, "Hello"), document.getElementById("root"));
