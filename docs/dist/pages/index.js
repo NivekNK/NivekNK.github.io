253f4d0fd5a17541aff7fd247e37fc68908d878d
Snowpack Build <actions@github.com>
2022-07-04 06:36:42 +0000

    Commit from GitHub Actions (Snowpack)

---
+++ docs/dist/pages/index.js
@@ -0,0 +1,3 @@
+import React from "../../snowpack/pkg/react.js";
+import ReactDOM from "../../snowpack/pkg/react-dom.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, "Hello"), document.getElementById("root"));
